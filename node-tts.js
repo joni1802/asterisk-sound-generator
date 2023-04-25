37369a1027fbeab7087e4bee5527586e104b9129
Jonathan <jonathan@francke.me>
2023-04-25 23:42:23 +0200

    wip: added creating sound files

--- node-tts.js
+++ node-tts.js
@@ -1,19 +1,39 @@
 import { execFile } from "node:child_process";
 import path from "node:path";
+import { parseSoundFile } from "./parse.js";
 
-const text = "Der Wabbel Wackel Armige Windhosen Kamerad";
-const modelName = "tts_models/de/thorsten/vits";
-const outPath = path.join("data", "test.wav");
+function init() {
+  const sounds = parseSoundFile("data\\core-sounds-de.txt");
+  const modelName = "tts_models/de/thorsten/vits";
 
-const tts = execFile(
-  "tts",
-  ["--text", text, "--model_name", modelName, "--out_path", outPath],
-  {
-    // This environment variables needs to be set. Otherwise the tts child process will throw an error.
-    // See https://stackoverflow.com/a/63573649
-    env: { PYTHONIOENCODING: "utf-8", PYTHONLEGACYWINDOWSSTDIO: "utf-8" },
-  },
-  (err, stdout) => {
-    console.log(stdout);
+  let counter = 0;
+
+  for (let { name, text } of sounds) {
+    if (counter === 10) {
+      break;
+    }
+
+    if (text) {
+      tts(text, modelName, path.join("data", `${name}.wav`));
+    }
+
+    counter++;
   }
-);
+}
+
+function tts(text, modelName, outPath) {
+  execFile(
+    "tts",
+    ["--text", text, "--model_name", modelName, "--out_path", outPath],
+    {
+      // This environment variables needs to be set. Otherwise the tts child process will throw an error.
+      // See https://stackoverflow.com/a/63573649
+      env: { PYTHONIOENCODING: "utf-8", PYTHONLEGACYWINDOWSSTDIO: "utf-8" },
+    },
+    (err, stdout) => {
+      console.log(stdout);
+    }
+  );
+}
+
+init();
