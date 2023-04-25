d9dc4029a1d29924a7eaedb8c9f111f340340471
Jonathan <jonathan@francke.me>
2023-04-25 23:33:44 +0200

    improved parser

--- parse.js
+++ parse.js
@@ -1,18 +1,10 @@
 import { readFileSync, writeFileSync } from "node:fs";
 
-function init() {
-  const coreSoundsEn = "core-sounds-en.txt";
-  const extraSoundsEn = "extra-sounds-en.txt";
-
-  parseSoundFile(coreSoundsEn, "core-sounds-en.json");
-  parseSoundFile(extraSoundsEn, "extra-sounds-en.json");
-}
-
-function parseSoundFile(filePath, output) {
-  const file = readFileSync(filePath, "utf8");
+export function parseSoundFile(sourceFile) {
+  const file = readFileSync(sourceFile, "utf8");
   const lines = file.split("\n");
 
-  const list = lines.map((line) => {
+  return lines.map((line) => {
     let [name, text] = line.split(":");
     let specialChars = /\[.*\]|\(.*\)|\<.*\>/;
 
@@ -20,19 +12,10 @@ function parseSoundFile(filePath, output) {
 
     return { name, text: text?.trim() };
   });
-
-  writeFileSync(output, JSON.stringify(list, null, 2), { flag: "wx" });
 }
 
-function test() {
-  return [
-    "Hey",
-    "activated: Activated.",
-    "added: Added.",
-    "an-error-has-occured: An error has occurred. (Symlink to 'an-error-has-occurred')",
-    "beep: [this is a simple beep tone]",
-    "confbridge-leave: <beep decending>",
-  ];
-}
+export function soundFileToJson(sourceFile, targetFile) {
+  const sounds = parseSoundFile(sourceFile);
 
-init();
+  writeFileSync(targetFile, JSON.stringify(sounds, null, 2), { flag: "wx" });
+}
