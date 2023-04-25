534fbff629c8bef87e62f1547d3c3681f718101e
Jonathan <jonathan@francke.me>
2023-04-25 19:14:13 +0200

    added translator and translated german core sound files

--- translate.js
+++ translate.js
@@ -1,15 +1,58 @@
 import * as dotenv from "dotenv";
 import * as deepl from "deepl-node";
+import { open, mkdir } from "node:fs/promises";
+import path from "node:path";
 
 dotenv.config();
+
+const debug = false;
 
 const authKey = process.env.DEEPL_AUTH_KEY;
 const translator = new deepl.Translator(authKey);
 
-const result = await translator.translateText(
-  "Please enter your personal identification number followed by the pound, or hash key.",
-  null,
-  "de"
-);
+async function init() {
+  translateFile("core-sounds-en.txt", "core-sounds-de.txt", "de");
+}
 
-console.log(result.text);
+async function translateFile(sourceFile, targetFile, targetLangCode) {
+  const source = await open(sourceFile);
+  const targetDir = "data";
+
+  mkdir(targetDir, { recursive: true });
+
+  const target = await open(path.join(targetDir, targetFile), "ax");
+
+  let lineNumber = 1;
+
+  for await (const line of source.readLines()) {
+    // Translate max. 1000 lines by default.
+    // Deepl Free allows max. 500.000 characters to translate.
+    if (lineNumber === 1000) {
+      break;
+    }
+
+    const [name, text] = line.split(":");
+
+    if (text) {
+      const translation = await translator.translateText(text, null, "de");
+
+      debug && console.log(translation);
+
+      await target.writeFile(`${name}:${translation.text}\n`);
+
+      logger(lineNumber, `${name}:${translation.text}`);
+    } else {
+      await target.writeFile(`${line}\n`);
+
+      logger(lineNumber, line);
+    }
+
+    lineNumber++;
+  }
+}
+
+function logger(lineNumber, line) {
+  console.log(`${lineNumber}\t${line}`);
+}
+
+init();
