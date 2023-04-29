37e5e89ccc2a2c9ce910dd9844501b3ebca5186f
Jonathan <jonathan@francke.me>
2023-04-29 14:06:36 +0200

    wip: node-tts

--- node-tts.js
+++ node-tts.js
@@ -1,41 +1,63 @@
 import { execFile } from "node:child_process";
 import path from "node:path";
 import { parseSoundFile } from "./parse.js";
+import { mkdir } from "node:fs/promises";
 
-function init() {
-  const sounds = parseSoundFile(path.join("data", "core-sounds-de.txt"));
+const debug = false;
+
+async function init() {
+  console.time();
+
+  const targetLangCode = "de";
+  const targetDir = `sounds/${targetLangCode}/core`;
+  const coreSounds = parseSoundFile(
+    path.join("transcriptions", `core-sounds-${targetLangCode}.txt`)
+  );
   const modelName = "tts_models/de/thorsten/vits";
 
-  console.log(sounds);
+  await mkdir(targetDir, { recursive: true });
 
-  let counter = 0;
+  // Just for testing
+  // let counter = 0;
 
-  for (let { name, text } of sounds) {
-    if (counter === 10) {
-      break;
-    }
+  for (let { name, text } of coreSounds) {
+    // if (counter === 50) {
+    //   break;
+    // }
 
     if (text) {
-      tts(text, modelName, path.join("data", `${name}.wav`));
+      await tts(text, modelName, path.join(targetDir, `${name}.wav`));
     }
 
-    counter++;
+    // counter++;
   }
+
+  console.timeEnd();
 }
 
 function tts(text, modelName, outPath) {
-  execFile(
-    "tts",
-    ["--text", text, "--model_name", modelName, "--out_path", outPath],
-    {
-      // This environment variables needs to be set. Otherwise the tts child process will throw an error.
-      // See https://stackoverflow.com/a/63573649
-      env: { PYTHONIOENCODING: "utf-8", PYTHONLEGACYWINDOWSSTDIO: "utf-8" },
-    },
-    (err, stdout) => {
-      console.log(stdout);
-    }
-  );
+  return new Promise((resolve, reject) => {
+    execFile(
+      "tts",
+      ["--text", text, "--model_name", modelName, "--out_path", outPath],
+      {
+        // This environment variables needs to be set. Otherwise the tts child process will throw an error.
+        // See https://stackoverflow.com/a/63573649
+        env: { PYTHONIOENCODING: "utf-8", PYTHONLEGACYWINDOWSSTDIO: "utf-8" },
+      },
+      (err, stdout) => {
+        debug && console.log(stdout);
+
+        if (err) {
+          reject(err);
+        }
+
+        console.log(outPath);
+
+        resolve();
+      }
+    );
+  });
 }
 
 init();
