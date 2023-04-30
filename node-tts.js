081c67b35c5b39e0d00d229bd19df55ae3c3bc51
Jonathan <jonathan@francke.me>
2023-04-30 12:54:37 +0200

    added google tts engine

--- node-tts.js
+++ node-tts.js
@@ -1,7 +1,11 @@
 import { execFile } from "node:child_process";
 import path from "node:path";
 import { parseSoundFile } from "./parse.js";
-import { mkdir } from "node:fs/promises";
+import { mkdir, writeFile } from "node:fs/promises";
+import googleTtsApi from "@google-cloud/text-to-speech";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const debug = false;
 
@@ -9,11 +13,12 @@ async function init() {
   console.time();
 
   const targetLangCode = "de";
-  const targetDir = `sounds/${targetLangCode}/core`;
+  const targetDir = path.join("sounds", targetLangCode, "core");
   const coreSounds = parseSoundFile(
     path.join("transcriptions", `core-sounds-${targetLangCode}.txt`)
   );
   const modelName = "tts_models/de/thorsten/vits";
+  const voiceName = "de-DE-Neural2-C";
 
   await mkdir(targetDir, { recursive: true });
 
@@ -21,15 +26,23 @@ async function init() {
   let counter = 0;
 
   for (let { name, text } of coreSounds) {
-    if (counter >= 125) {
-      if (text) {
-        if (name.split("/").length > 1) {
-          let subdir = name.split("/").slice(0, -1);
+    if (counter === 10) {
+      break;
+    }
 
-          mkdir(path.join(targetDir, ...subdir), { recursive: true });
-        }
-        await tts(text, modelName, path.join(targetDir, `${name}.wav`));
+    if (text) {
+      if (name.split("/").length > 1) {
+        let subdir = name.split("/").slice(0, -1);
+
+        mkdir(path.join(targetDir, ...subdir), { recursive: true });
       }
+      // await coquiTts(text, modelName, path.join(targetDir, `${name}.wav`));
+      await googleTts(
+        text,
+        `${targetLangCode}-DE`,
+        voiceName,
+        path.join(targetDir, `${name}.mp3`)
+      );
     }
 
     counter++;
@@ -38,11 +51,11 @@ async function init() {
   console.timeEnd();
 }
 
-function tts(text, modelName, outPath) {
+function coquiTts(text, modelName, targetFile) {
   return new Promise((resolve, reject) => {
     execFile(
       "tts",
-      ["--text", text, "--model_name", modelName, "--out_path", outPath],
+      ["--text", text, "--model_name", modelName, "--out_path", targetFile],
       {
         // This environment variables needs to be set. Otherwise the tts child process will throw an error.
         // See https://stackoverflow.com/a/63573649
@@ -55,12 +68,36 @@ function tts(text, modelName, outPath) {
           reject(err);
         }
 
-        console.log(outPath);
+        console.log(targetFile);
 
         resolve();
       }
     );
   });
+}
+
+async function googleTts(text, langCode, voiceName, targetFile) {
+  const client = new googleTtsApi.TextToSpeechClient();
+
+  const request = {
+    input: {
+      text,
+    },
+    voice: {
+      languageCode: langCode,
+      name: voiceName,
+    },
+    audioConfig: { audioEncoding: "MP3" },
+  };
+
+  const [response] = await client.synthesizeSpeech(request);
+
+  await writeFile(targetFile, response.audioContent, {
+    flag: "wx",
+    encoding: "binary",
+  });
+
+  console.log(targetFile);
 }
 
 init();
