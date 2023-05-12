024c382a22bb42fb4b20ea4a6b8f5cd1adab58c7
Jonathan <jonathan@francke.me>
2023-05-12 14:24:23 +0200

    wip: added chatgpt translation

--- translate.js
+++ translate.js
@@ -1,14 +1,17 @@
 import * as dotenv from "dotenv";
 import * as deepl from "deepl-node";
-import { open, mkdir, readdir } from "node:fs/promises";
+import { open, mkdir, readdir, readFile } from "node:fs/promises";
 import path from "node:path";
 import inquirer from "inquirer";
+import { OpenAI } from "openai-streams/node";
+import { createWriteStream } from "node:fs";
+import { encode, decode } from "gpt-3-encoder";
 
 dotenv.config();
 
 const debug = false;
 
-async function translateFile(sourceFile, targetFile, targetLangCode) {
+async function deeplTranslateFile(sourceFile, targetFile, targetLangCode) {
   const authKey = process.env.DEEPL_AUTH_KEY;
   const translator = new deepl.Translator(authKey);
   const source = await open(sourceFile);
@@ -57,6 +60,63 @@ function getTargetFilename(sourceFile, targetLangCode) {
   return sourceFile.replace(regex, `$1${targetLangCode}$2`);
 }
 
+async function openaiTranslateFile(sourceFile, targetFile, language) {
+  return new Promise(async (resolve, reject) => {
+    const source = await readFile(sourceFile, { encoding: "utf8" });
+    const target = createWriteStream(targetFile, { flags: "a" });
+    const content = `Translate a transcription of a phone system to ${language}. Keep the original formatting and only translate everything after the colon on each line. Always translate the word "extension" to the german word "Durchwahl". 
+Here is the transcription:
+${source}
+`;
+
+    const stream = await OpenAI(
+      "chat",
+      { model: "gpt-3.5-turbo", messages: [{ role: "user", content }] },
+      { apiKey: process.env.OPEN_AI_AUTH_KEY }
+    );
+
+    stream.setEncoding("utf8");
+
+    stream.pipe(target, { end: false });
+
+    stream.on("data", (data) => {
+      process.stdout.write(data);
+    });
+
+    stream.on("end", () => {
+      console.log("\n");
+
+      target.end("\n", resolve);
+    });
+
+    stream.on("error", reject);
+  });
+}
+
+function getTranscriptionChunks(transcription, tokenLimit) {
+  const tokens = encode(transcription);
+
+  const chunks = [];
+
+  let lastLine = "";
+
+  for (let i = 0; i < tokens.length; i = i + tokenLimit) {
+    const chunk = tokens.slice(i, i + tokenLimit);
+
+    let decoded = lastLine + decode(chunk);
+
+    const lines = decoded.split("\n");
+
+    lastLine = lines[lines.length - 1];
+
+    decoded = decoded.replace(lastLine, "");
+
+    chunks.push(decoded);
+  }
+
+  return chunks;
+}
+
 function logger(lineNumber, line) {
   console.log(`${lineNumber}\t${line}`);
 }
@@ -66,9 +126,27 @@ export default async function init() {
 
   const questions = [
     {
+      type: "list",
+      name: "translator",
+      message: "Choose the translation engine.",
+      choices: [
+        {
+          name: "ChatGPT",
+          value: "chatgpt",
+        },
+        {
+          name: "DeepL",
+          value: "deepl",
+        },
+      ],
+    },
+    {
       type: "rawlist",
       name: "targetLangCode",
       message: "Select the language to translate into.",
+      when(answers) {
+        return answers.translator === "deepl";
+      },
       async choices() {
         const languages = await getTargetLanguages();
 
@@ -79,6 +157,24 @@ export default async function init() {
           };
         });
       },
+    },
+    {
+      type: "rawlist",
+      name: "targetLanguage",
+      message: "Select the language to translate into.",
+      when(answers) {
+        return answers.translator === "chatgpt";
+      },
+      // to-do: improve list and maybe combine list with deepls one.
+      choices: [
+        {
+          name: "German",
+          value: {
+            code: "de",
+            name: "german",
+          },
+        },
+      ],
     },
     {
       type: "rawlist",
@@ -102,7 +198,7 @@ export default async function init() {
     return;
   }
 
-  const targetLangCode = answers.targetLangCode;
+  const targetLangCode = answers.targetLangCode || answers.targetLanguage.code;
   const sourceFile = path.join(targetDir, answers.sourceFile);
   const targetFile = path.join(
     targetDir,
@@ -111,5 +207,15 @@ export default async function init() {
 
   mkdir(targetDir, { recursive: true });
 
-  await translateFile(sourceFile, targetFile, targetLangCode);
+  switch (answers.translator) {
+    case "deepl":
+      await deeplTranslateFile(sourceFile, targetFile, targetLangCode);
+      break;
+    case "chatgpt":
+      await openaiTranslateFile(
+        sourceFile,
+        targetFile,
+        answers.targetLanguage.name
+      );
+  }
 }
